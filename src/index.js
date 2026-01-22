72eb959d6aba4cbf54b839ff190d9e5da929f007
Chanpreet Sembhi <157610640+chanpreetsembhi@users.noreply.github.com>
2026-01-22 06:22:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ mongoose
 
     app.use(
       cors({
-        origin: "https://nep-dash.vercel.app",
+        origin: ["https://nep-dash.vercel.app", "http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
